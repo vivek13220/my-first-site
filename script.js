976b37930cc2c6c30d102e0a4d5c3369e0125945
vivek13220 <princevivek13220@gmail.com>
2021-09-15 20:12:52 -0600

    my first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    var button = document.querySelector('button');
+  
+    button.addEventListener('click', function() {
+      alert("Let's Rock!!");
+    });
+  }
+  
